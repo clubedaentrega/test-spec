7d064437b82b42f93fabb8827580e37f3d61cf0e
Guilherme Souza <sitegui@sitegui.com.br>
2016-01-20 02:07:16 -0200

    Prepare docs

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+/** @module */
 'use strict'
 
+/**
+ * Parse
+ * @see module:lib/parse
+ */
 module.exports.parse = require('./lib/parse')
+
+/**
+ * Compile
+ * @see module:lib/compile
+ */
 module.exports.compile = require('./lib/compile')
+
+/**
+ * Base random utilities
+ * @see module:lib/baseContext
+ */
 module.exports.baseContext = require('./lib/baseContext')
+
+/**
+ * Extract code snippet
+ * @see module:lib/getSnippet
+ */
 module.exports.getSnippet = require('./lib/getSnippet')
